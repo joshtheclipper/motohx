fea229b60e227a071da5b1c4652ae33aa709f0f8
joshtheclipper <joshkenkel@gmail.com>
2026-06-07 18:27:34 -0400

    Day 2: changed direction to a webapp that track car maintenance

--- app.py
+++ app.py
@@ -1,11 +1,25 @@
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template, request
 
 app = Flask(__name__)
 
+maintenance_records = []
 
-@app.route("/")
+
+@app.route("/", methods=["GET", "POST"])
 def home():
-    return render_template("index.html")
+
+    if request.method == "POST":
+        new_date = request.form.get("date")
+        new_service = request.form.get("service")
+        new_mileage = request.form.get("mileage")
+
+        new_record = {"date": new_date, "service": new_service, "mileage": new_mileage}
+
+        maintenance_records.append(new_record)
+
+        return redirect("/")
+
+    return render_template("index.html", logs=maintenance_records)
 
 
 if __name__ == "__main__":
